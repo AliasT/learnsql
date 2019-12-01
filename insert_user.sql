847b257cc2ccc12d0df5338291f1e62273d5165c
chaixb <chai_xb@163.com>
2019-12-01 23:17:07 +0800

    batch insert

--- insert_user.sql
+++ insert_user.sql
@@ -1,3 +1,7 @@
 use learnsql;
 
 insert into users (nickname, pass) values ("xiaobing", "123456");
+
+/* batch insert */
+insert into users (nickname, pass) values ("jordan", "123456"), ("anonymous", "anonymous");
+
