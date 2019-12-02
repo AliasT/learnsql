33f05d053eb0442850093c63bb1d7ca213f1cebf
chaixb <chai_xb@163.com>
2019-12-02 23:13:27 +0800

    add a script

--- insert_user.sql
+++ insert_user.sql
@@ -2,6 +2,9 @@ use learnsql;
 
 insert into users (nickname, pass) values ("xiaobing", "123456");
 
+/* insert if not exists */
+insert into learnsql.users (nickname, pass) select * from  (select "xiaobing2", "123456") as tmp where not exists (select nickname from learnsql.users where nickname = "xiaobing2") limit 1;
+
 /* batch insert */
 insert into users (nickname, pass) values ("jordan", "123456"), ("anonymous", "anonymous");
 
